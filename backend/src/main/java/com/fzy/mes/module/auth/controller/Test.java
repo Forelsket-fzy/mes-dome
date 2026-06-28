3639504742bdc24c7a0eef4196635d1c13ce2e8d
f1805 <f18055762387@gmail.com>
2026-06-28 16:51:10 +0800

    day2:Entity/MapperScan GlobalExceptionHandler Result

---
+++ backend/src/main/java/com/fzy/mes/module/auth/controller/Test.java
@@ -0,0 +1,19 @@
+package com.fzy.mes.module.auth.controller;
+
+
+import com.fzy.mes.common.module.vo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Test {
+
+    @GetMapping("/Test")
+    public Result<String> Test1(){
+        System.out.println("Test");
+        return Result.success("test");
+    }
+
+}
